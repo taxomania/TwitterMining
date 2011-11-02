78c6e2858a58089810b7e14a9690dfb04876925b
Tariq Patel <tariq.patel@gmail.com>
2011-11-02 11:49:03 +0000

    Removed files unused in branch

--- src/uk/ac/manchester/cs/patelt9/twitter/ParseListener.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.manchester.cs.patelt9.twitter;
-
-public interface ParseListener {
-    void onParseComplete(Tweet t);
-    void onParseComplete(long id);
-} // ParseListener
--- src/uk/ac/manchester/cs/patelt9/twitter/StreamListener.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.manchester.cs.patelt9.twitter;
-
-import com.google.gson.JsonObject;
-
-public interface StreamListener {
-    void onJsonReadComplete(JsonObject jo);
-} // StreamListener
