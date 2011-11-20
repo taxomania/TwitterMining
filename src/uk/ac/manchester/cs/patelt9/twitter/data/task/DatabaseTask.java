b020b01d4da21f8e483b4cfc997bf741a432e595
Tariq Patel <tariq.patel@gmail.com>
2011-11-20 17:57:03 +0000

    Removed javadoc from repo

--- src/uk/ac/manchester/cs/patelt9/twitter/data/task/DatabaseTask.java
+++ src/uk/ac/manchester/cs/patelt9/twitter/data/task/DatabaseTask.java
@@ -2,7 +2,24 @@ package uk.ac.manchester.cs.patelt9.twitter.data.task;
 
 import uk.ac.manchester.cs.patelt9.twitter.data.DatabaseConnector;
 
+/**
+ * Any database tasks to be carried out must implement this interface.
+ *
+ * @author Tariq Patel
+ */
 public interface DatabaseTask {
+    /**
+     *
+     * @return String representation of task
+     */
     public String toString();
+
+    /**
+     * Carry out the task specific to the implementing task
+     *
+     * @param db
+     *            DatabaseConnector object
+     * @return The number of rows affected
+     */
     public int doTask(DatabaseConnector db);
 } // DatabaseTask
