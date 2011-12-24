f49a7e988866e99fad2c79040ca04ce73ff47d2f
Tariq Patel <tariq.patel@gmail.com>
2011-12-24 17:02:34 +0000

    Tweaked lc.sh

--- lc.sh
+++ lc.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-echo "---------------------"
+LINEBREAK="---------------------"
+echo $LINEBREAK
 
 LINES=`wc -l \`find pysrc -name *.py\` | grep "total"`
 let "COUNT=${LINES/total/}"
-printf 'Python:\t%7s lines\n' $COUNT
+printf 'Python:\t%7d lines\n' $COUNT
 
 LINES=`wc -l \`find src -name *.java\` | grep "total"`
 let "COUNT2=${LINES/total/}"
-printf 'Java:\t%7s lines\n' $COUNT2
+printf 'Java:\t%7d lines\n' $COUNT2
 
-echo "---------------------"
+echo $LINEBREAK
 
 let "COUNT += COUNT2"
-printf "Total:\t%7s lines\n" $COUNT
+printf "Total:\t%7d lines\n" $COUNT
 
-echo "---------------------"
+echo $LINEBREAK
