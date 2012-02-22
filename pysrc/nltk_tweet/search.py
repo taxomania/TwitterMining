a0541b8193f803b0358661b2ab5a293430b92460
Tariq Patel <tariq.patel@gmail.com>
2012-02-22 12:39:06 +0000

    Take command line arguments for db search

--- pysrc/nltk_tweet/search.py
+++ pysrc/nltk_tweet/search.py
@@ -3,15 +3,21 @@ Created on Feb 21, 2011
 
 @author: Tariq Patel
 '''
-
 import sys
 
 from database_connector import MongoConnector
 
 def main():
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print "Please provide search terms"
+        return 1
     mongo = MongoConnector()
-    cursor = mongo.find("Adium")
-    print cursor.count()
+    cursors = []
+    for word in args:
+        cursor = mongo.find(word)
+        print word, cursor.count()
+        cursors.append(cursor)
     return 0
 
 if __name__ == '__main__':
