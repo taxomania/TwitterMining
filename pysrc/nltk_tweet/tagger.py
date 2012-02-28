74ce758c694a76b8f587edcf8c0f52c556f01fd7
Tariq Patel <tariq.patel@gmail.com>
2012-02-28 18:09:02 +0000

    Able to find new software based on regex rules, not using db yet, fixed some ngram bugs, renamed variables to signify internal behaviour

--- pysrc/nltk_tweet/tagger.py
+++ pysrc/nltk_tweet/tagger.py
@@ -19,12 +19,13 @@ from utils import Dictionary, IncompleteTaggingError
 class TweetTagger(object):
     def __init__(self):
         super(TweetTagger, self).__init__()
-        self.sql = SQLConnector()
-        #self.mongo = MongoConnector()
+        self._sql = SQLConnector()
+        #self._mongo = MongoConnector()
 
     def _tag(self, tweet):
         tweet_id = str(tweet[0])
         text = tweet[1].lower()
+        #text = "download holiday havoc in itunes"
 
         urls = find_url(text)
         for url in urls:
@@ -35,11 +36,16 @@ class TweetTagger(object):
         words = regexp_tokenize(text, pattern=r'\w+([.,]\w+)*|\S+')
         #print words
         prices = find_price(words)
-        print prices
+
         pos_ = pos(words)
 
         #ngrams_ = self._ngrams(pos_, 2)
-        five_gram = ngrams(pos_, 5)
+        n = 5
+        five_gram = None
+        while not five_gram:
+            # robustness for when tweet length is less than n
+            five_gram = ngrams(pos_, n)
+            n -= 1
         #print five_gram
 
         tagged_tweet = self._ngram_tagger(five_gram, tweet_id)
@@ -56,69 +62,64 @@ class TweetTagger(object):
 
         for tagged_words in ngram:
             self._tagger(tagged_words, tags)
-
         return tags
 
     def _tagger(self, gram, tags):
         words = []
         tags_ = []
         phrase = ""
+        pos_soft = ""
+        possible_software = False
+        # Compile regular expressions outside of for loop
+        # for efficiency purposes
+        free_price = re.compile(r'free', re.IGNORECASE)
+        check_is = re.compile(r'is', re.IGNORECASE)
+        check_get = re.compile(r'download|get', re.IGNORECASE)
         for tagged_word in gram:
             word = tagged_word[0]
+            tag = tagged_word[1]
             phrase += word + " "
-            if re.match(re.compile(r'free', re.IGNORECASE), word):
-                #print word
-                #print words
+            if possible_software:
+                if tagIsNoun(tag):
+                    pos_soft += word + " "
+                    if word == gram[len(gram)-1][0]:
+                        pos_soft = ""
+                else:
+                    possible_software = False
+            if re.match(free_price, word):
                 try:
                     prev = words.pop()
                     words.append(prev)
-                    print tags
-                    if re.match(re.compile(r'is', re.IGNORECASE), prev):
+                    if re.match(check_is, prev):
                         tags.add('price', word)
                     else:
                         prev = tags_.pop()
                         tags_.append(prev)
-                        print prev
                         if tagIsNoun(prev):
                             tags.add('price', word)
-                            print prev_tag
-                            print tags
-                    print tags
                 except:
+                    # This is first word in phrase
                     pass
+            elif re.match(check_get, word):
+                possible_software = True
+
+
+            # Back in main part of loop
             words.append(word)
-            tags_.append(tagged_word[1])
+            tags_.append(tag)
+
+        # End of for loop
+
         phrase = phrase.strip()
-        #print words
-        #print tags
-        #print phrase
+        if len(pos_soft) > 0:
+            pos_soft = pos_soft.strip()
+            tags.add('software_name', pos_soft)
 
-        regex = re.compile(r'get[\w.\s]+', re.IGNORECASE)
-        #('free')
-        matches = re.findall(regex, phrase)
-        if len(matches) > 0:
-            print matches
-
-    '''
-    def _ngrams_tagger(self, ngrams_, tweet_id):
-        tags = Dictionary()
-        tags.add('tweet_db_id', tweet_id)
-
-        for i in range(len(ngrams_), 0, -1):
-            for words in ngrams_[i]:
-                self._tagger(words, tags)
-        return tags
-
-    def _ngrams(self, pos_words, max_n):
-        ngram = {}
-        for n in range(1, max_n+1):
-            ngram[n] = ngrams(pos_words, n)
-        return ngram
-    '''
+        # CHECK DB HERE? OR ABOVE
 
     def tag(self, pages):
         for page in xrange(pages):
-            res = self.sql.load_data(page)
+            res = self._sql.load_data(page)
             rows = res.num_rows()
             if not rows:
                 print "No tweets left to analyse"
@@ -136,8 +137,8 @@ class TweetTagger(object):
                         print
 
     def close(self):
-        self.sql.close()
-        #self.mongo.close()
+        self._sql.close()
+        #self._mongo.close()
 
 def main():
     tagger = TweetTagger()
