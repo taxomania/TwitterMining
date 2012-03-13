d4db8570183bbff1c16c7a085894c7c36a654c81
Tariq Patel <tariq.patel@gmail.com>
2012-03-13 20:27:52 +0000

    Refactored web.py for organisation and readability

--- pysrc/nltk_tweet/web.py
+++ pysrc/nltk_tweet/web.py
@@ -45,38 +45,14 @@ class Web(object):
         self._imgc = None
         self._auth = False
 
-    @cherrypy.expose
-    def twitter(self):
-        self._page='../twitter'
-        if self._auth:
-            return self._get_template('search.html', action='../twittersearch')
-        raise cherrypy.HTTPRedirect('../auth')
+    ''' TEMPLATE HELPER FUNCTIONS '''
+    def _get_template(self, file, **kwargs):
+        kwargs.update(self._nav)
+        return self._tmpl.get_template(file).render(**kwargs)
 
-    @cherrypy.expose
-    def twittersearch(self, query):
-        if not self._auth:
-            raise cherrypy.HTTPRedirect('../auth')
-        return self._template(body="Searching Twitter..." + JavaScript.redirect('twitter/%s' % query))
-
-    @cherrypy.expose
-    def tweets(self, query):
-        if not self._auth:
-            raise cherrypy.HTTPRedirect('../../auth')
-        tweets = self._search_twitter(query)
-        if len(tweets):
-            return self._get_template('search_tweet.html', tweets=tweets)
-        else:
-            return self._template(body='No tweets were found')
-
-    def _search_twitter(self, query):
-        tweets_ = subprocess.check_output(self._java_search + query, shell=True).strip().decode('utf-8', 'ignore').split('\n')
-        tweets = []
-        for tweet in tweets_:
-            tweet_ = tweet.split('\t')
-            if len(tweet_) > 1:
-                tweets.append(tweet_)
-        print tweets
-        return tweets
+    def _template(self, body):
+        return Template('<%inherit file="base.html"/>'+body,lookup=self._tmpl).render(**self._nav)
+    ''' END TEMPLATE HELPER FUNCTIONS '''
 
     @cherrypy.expose
     def index(self):
@@ -87,6 +63,13 @@ class Web(object):
         body += "been authenticated"
         return self._get_template('index.html', body=body)
 
+    ''' AUTH '''
+    @cherrypy.expose
+    def auth(self):
+        if self._auth:
+            return self._get_template('logout.html', action='../logout')
+        return self._get_template('auth.html', action='../ssh', mport=28817, sport=3307)
+
     @cherrypy.expose
     def logout(self):
         self._sql.close()
@@ -95,90 +78,11 @@ class Web(object):
         raise cherrypy.HTTPRedirect('../auth')
 
     @cherrypy.expose
-    def auth(self):
-        if self._auth:
-            return self._get_template('logout.html', action='../logout')
-        return self._get_template('auth.html', action='../ssh', mport=28817, sport=3307)
-
-    @cherrypy.expose
-    def tag(self):
-        self._page='../tag'
-        if self._auth:
-            return self._template(body="Extracting features" + JavaScript.redirect('../tagger'))
-        raise cherrypy.HTTPRedirect('../auth')
-
-    @cherrypy.expose
-    def example(self):
-        self._page='../example'
-        if self._auth:
-            return self._get_template('search_example.html', action='../tweet')
-        raise cherrypy.HTTPRedirect('../auth')
-
-    @cherrypy.expose
-    def tweet(self, tweet_id=None):
-        if not self._auth:
-            raise cherrypy.HTTPRedirect('../auth')
-        if tweet_id.isdigit():
-            return self._template(body="Loading..." + JavaScript.redirect('tweet/%s' % tweet_id))
-        raise cherrypy.HTTPRedirect(self._page)
-
-    @cherrypy.expose
-    def examples(self, tweet_id=None):
-        if not self._auth:
-            raise cherrypy.HTTPRedirect('../../auth')
-        if not tweet_id.isdigit():
-            raise cherrypy.HTTPRedirect(self._page)
-        tagger = TweetTagger(sql=self._sql, mongo=self._mongo)
-        out = tagger.tag_by_tweet_id(tweet_id)
-        if not out:
-            return self._template(body='Tweet not found')
-        return self._get_template('tweet.html', tweets=[out])
-
-    @cherrypy.expose
-    def results(self):
-        self._page='../results'
-        if self._auth:
-            return self._template(body='Retrieving data'+ JavaScript.redirect('../analyse'))
-        raise cherrypy.HTTPRedirect('../auth')
-
-    @cherrypy.expose
-    def aggregate(self, name):
-        if not self._imgc:
-            raise cherrypy.HTTPRedirect('../../auth')
-        data, col = self._imgc.web_query(name)
-        if not len(data):
-            return self._template(body='That software has not been found')
-        return self._get_template(file='google-charts.html', button=self._page, title=name, data=data, colours=col)
-
-    @cherrypy.expose
-    def analyse(self):
-        if not self._auth:
-            raise cherrypy.HTTPRedirect('../auth')
-        elements = self._mongo.find_all_software_os()
-        if not len(elements):
-            return self._template(body='No software has been found yet')
-        return self._get_template(file='show_results.html', action='../analysis', elements=elements)
-
-    @cherrypy.expose
-    def analysis(self, software):
-        if not self._auth:
-            raise cherrypy.HTTPRedirect('../auth')
-        raise cherrypy.HTTPRedirect('analysis/%s' % software)
-
-    def _get_template(self, file, **kwargs):
-        kwargs.update(self._nav)
-        return self._tmpl.get_template(file).render(**kwargs)
-
-    def _template(self, body):
-        return Template('<%inherit file="base.html"/>'+body,lookup=self._tmpl).render(**self._nav)
-
-    @cherrypy.expose
     def ssh(self, user, passwd, db, host=None,
             sport=3306, mport=27017, local=False):
         if not (user and passwd and db):
                 return 'Please fill in all details' + JavaScript.timed_redirect(location='../',
                                                                                 time=1500)
-
         if not local:
             if not host:
                 return 'Please provide host address' + JavaScript.timed_redirect(location='../',
@@ -207,14 +111,120 @@ class Web(object):
         self._imgc = ImgCreator(mongo=self._mongo)
 
         raise cherrypy.HTTPRedirect(self._page)
+    ''' END AUTH '''
+
+    ''' TWITTER SEARCH API '''
+    @cherrypy.expose
+    def twitter(self):
+        self._page='../twitter'
+        if self._auth:
+            return self._get_template('search.html', action='../twittersearch')
+        raise cherrypy.HTTPRedirect('../auth')
+
+    @cherrypy.expose
+    def twittersearch(self, query):
+        if self._auth:
+            return self._template(body="Searching Twitter..." + JavaScript.redirect('twitter/%s' % query))
+        raise cherrypy.HTTPRedirect('../auth')
+
+    @cherrypy.expose
+    def tweets(self, query):
+        if not self._auth:
+            raise cherrypy.HTTPRedirect('../../auth')
+        tweets = self._search_twitter(query)
+        if len(tweets):
+            return self._get_template('search_tweet.html', tweets=tweets)
+        return self._template(body='No tweets were found')
+
+    def _search_twitter(self, query):
+        tweets_ = subprocess.check_output(self._java_search + query, shell=True).strip().decode('utf-8', 'ignore').split('\n')
+        tweets = []
+        for tweet in tweets_:
+            tweet_ = tweet.split('\t')
+            if len(tweet_) > 1:
+                tweets.append(tweet_)
+        print tweets
+        return tweets
+    ''' END TWITTER SEARCH API '''
+
+    ''' FEATURE EXTRACTION '''
+    @cherrypy.expose
+    def tag(self):
+        self._page='../tag'
+        if self._auth:
+            return self._template(body="Extracting features" + JavaScript.redirect('../tagger'))
+        raise cherrypy.HTTPRedirect('../auth')
 
     @cherrypy.expose
     def tagger(self):
         if not self._auth:
             raise cherrypy.HTTPRedirect('../auth')
         tagger = TweetTagger(sql=self._sql, mongo=self._mongo)
-
         return self._get_template('tweet.html', tweets=tagger.tag(2))
+    ''' END FEATURE EXTRACTION '''
+
+    ''' EXAMPLE PAGE '''
+    @cherrypy.expose
+    def example(self):
+        self._page='../example'
+        if self._auth:
+            return self._get_template('search_example.html', action='../tweet')
+        raise cherrypy.HTTPRedirect('../auth')
+
+    @cherrypy.expose
+    def tweet(self, tweet_id=None):
+        if not self._auth:
+            raise cherrypy.HTTPRedirect('../auth')
+        if tweet_id.isdigit():
+            return self._template(body="Loading..." + JavaScript.redirect('tweet/%s' % tweet_id))
+        raise cherrypy.HTTPRedirect(self._page)
+
+    @cherrypy.expose
+    def examples(self, tweet_id=None): # routed as /tweet/:tweet_id
+        if not self._auth:
+            raise cherrypy.HTTPRedirect('../../auth')
+        if not tweet_id.isdigit():
+            raise cherrypy.HTTPRedirect(self._page)
+        tagger = TweetTagger(sql=self._sql, mongo=self._mongo)
+        out = tagger.tag_by_tweet_id(tweet_id)
+        if not out:
+            return self._template(body='Tweet not found')
+        return self._get_template('tweet.html', tweets=[out])
+    ''' END EXAMPLE PAGE '''
+
+    ''' ANALYSIS '''
+    @cherrypy.expose
+    def results(self): # This page loads data for analysis
+        self._page='../results'
+        if self._auth:
+            return self._template(body='Retrieving data'+ JavaScript.redirect('../analyse'))
+        raise cherrypy.HTTPRedirect('../auth')
+
+    @cherrypy.expose
+    def analyse(self):
+        if not self._auth:
+            raise cherrypy.HTTPRedirect('../auth')
+        elements = self._mongo.find_all_software_os()
+        if not len(elements):
+            return self._template(body='No software has been found yet')
+        return self._get_template(file='show_results.html', action='../analysis', elements=elements)
+
+    @cherrypy.expose
+    def analysis(self, software):
+        if not self._auth:
+            raise cherrypy.HTTPRedirect('../auth')
+        raise cherrypy.HTTPRedirect('analysis/%s' % software)
+
+    @cherrypy.expose
+    def aggregate(self, name): # routed as /analysis/:name
+        if not self._imgc:
+            raise cherrypy.HTTPRedirect('../../auth')
+        data, col = self._imgc.web_query(name)
+        if not len(data):
+            return self._template(body='That software has not been found')
+        return self._get_template(file='google-charts.html', button=self._page, title=name, data=data, colours=col)
+    ''' END ANALYSIS '''
+
 
 def setup_routes(cp=None):
     w = Web(dirs=['web'], java_classpath=cp)
